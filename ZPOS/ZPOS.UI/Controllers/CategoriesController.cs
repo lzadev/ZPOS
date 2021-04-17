3fd0204cb8b577af6486abfd28b46c93787531ea
LuisZabala10 <zabalaluis1234@gmail.com>
2021-04-17 11:13:55 -0400

    Bug corregido en el formulario Editar Producto.
    Corregi el error que habia que el status del producto seimpre me llegaba false a la accion EditProduct

---
+++ ZPOS/ZPOS.UI/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZPOS.UI.Interfaces;
+
+namespace ZPOS.UI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryServices _categoryServices;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryServices categoryServices, IMapper mapper)
+        {
+            _categoryServices = categoryServices ?? throw new ArgumentNullException(nameof(categoryServices));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
